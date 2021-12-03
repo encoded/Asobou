1c47a763a944fdece5a11c5c792d7b0a80bf6f88
numiaproject <cosimo@Cosimos-MacBook-Pro.local>
2021-12-03 20:15:31 +0000

    Canvas, html fillable text, JSON and donwload
    Added canvas and html fillable text, example of json file for picture info, implemented a way to download the json of the modified picture data as text file.

--- docs/js/storycubes.js
+++ docs/js/storycubes.js
@@ -1,3 +1,30 @@
+//---------THIS DOWNLOAD THE FILE-----------//
+function download(content, fileName, contentType){
+    var a = document.createElement("a");
+    var file = new Blob([content], {type: contentType});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}
+
+//on click press this
+//download(JSON.stringify(flowerInfo, null, 2), 'json.txt', 'text/plain');
+
+//--------JSON data for picture-------------//
+let flowerInfo = new Object();
+$.getJSON("./data/storycubes/json/flower_info.json", function(flowerInfo) {
+    console.log(flowerInfo); // this will show the info it in firebug console
+
+	flowerInfo.verbs[0] = "ciccia";
+
+	textVerbs[textVerbs.length -1].innerHTML = flowerInfo.verbs[0];
+});
+
+//let's save the file before we unload
+window.onbeforeunload = function(){
+	flowerInfo.verbs[0] = "Ciccia";
+ }
+
 //-------PICTURE DATA--------------//
 //this requires: picturedata.js
 
@@ -38,7 +65,7 @@ camera.add( light );
 let group = new THREE.Group();
 group.position.x = 0 - w/2; //becuase camera is looking at 0, 0, 0
 group.position.y = 0 + h/2;
-scene.add( group );
+group.position.z = -1; //apparently if it's zero it is behind the camera
 
 //global group variable
 let numRow = 9;
@@ -46,6 +73,90 @@ let numColomn = 6;
 let size  = 40; //size box grid
 let margin = 10; //margin between boxes and frame
 
+//---------------TEXT INFO-------------------//
+//text to write in image info cavas
+let canvasWrap = document.createElement('div');
+let canvas = document.createElement('canvas');
+
+let div = document.createElement('div');
+
+let divVerbs = document.createElement('div');
+divVerbs.innerHTML = 'Verbs';
+let textVerbs = [];
+textVerbs.push(document.createElement('div'));
+textVerbs[textVerbs.length-1].innerHTML = "Temp";
+
+textVerbs.push(document.createElement('div'));
+textVerbs[textVerbs.length-1].innerText = "Hashiru";
+
+for (let index = 0; index < textVerbs.length; index++) {
+	let verb = textVerbs[index];
+	verb.contentEditable = true;
+	divVerbs.appendChild(verb);
+}
+
+div.appendChild(divVerbs);
+// div.innerText = 
+// 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer metus tortor, aliquet at sapien sed, porta consequat leo. Sed elementum congue magna nec malesuada. Vestibulum volutpat ligula accumsan, consequat eros et, accumsan tellus. Nunc eu luctus sem, id blandit dui. Vivamus et lorem vulputate, tristique justo pharetra, consequat felis. Maecenas varius ut nisl quis pellentesque. Suspendisse enim purus, lobortis sed ullamcorper eget, imperdiet ac ligula. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis ac semper est, non vestibulum arcu. In luctus ligula ac elementum venenatis. Duis volutpat sollicitudin pretium. In sed nisi ac turpis dictum dictum. Donec eget faucibus nisi. In felis quam, finibus ac neque tempor, tincidunt dignissim justo. Integer dolor ex, egestas vel orci at, consequat porta tellus. Nam commodo, tortor ac mattis placerat, mi neque bibendum velit, et varius elit nisi sed ex. Nullam ipsum purus, vehicula ut velit vehicula, blandit volutpat elit. Fusce finibus ante enim, sed aliquam velit pretium vel. Ut accumsan arcu turpis, a egestas leo eleifend eget. Duis auctor ultrices nulla eget blandit. Cras congue magna quis felis convallis, non volutpat odio fermentum. Donec fringilla luctus enim, ac accumsan ante. Phasellus tincidunt quis nibh sit amet vehicula. Vivamus ac condimentum turpis. Aliquam erat volutpat. Integer eget facilisis est, aliquet lacinia arcu. Phasellus bibendum arcu lectus, non condimentum urna placerat et. Curabitur in erat vitae enim suscipit tincidunt id ac mauris. Fusce tristique elit et diam hendrerit varius. Donec finibus blandit arcu in vehicula. Ut pretium mauris non quam aliquet, a auctor est commodo. Quisque vel elit ac arcu pretium hendrerit sed id lacus. Maecenas placerat metus eget purus commodo dapibus. Quisque mollis, dolor eget iaculis imperdiet, magna ipsum rutrum orci, ac pellentesque ligula massa et augue. Nullam consectetur dictum mauris ac condimentum. Etiam vehicula lacus turpis, eget vehicula magna interdum ac. Fusce justo lectus, sodales nec orci sed, mattis pulvinar turpis. Maecenas auctor scelerisque nisl in egestas. Etiam sit amet neque id eros mollis suscipit sed eu orci. Nulla eget diam eget orci lobortis egestas nec a enim. Praesent quis euismod sapien. Sed sed felis non mauris rutrum maximus sit amet sed mi. Duis convallis metus mi, ut posuere odio malesuada in. Vestibulum porta imperdiet augue vitae suscipit. Donec at placerat libero. Pellentesque sollicitudin justo imperdiet lorem consequat interdum. Donec nisl nisl, lacinia in lectus egestas, iaculis consectetur enim. Maecenas facilisis leo nec sem porta, ut viverra felis placerat.';
+// div.contentEditable = true;
+
+canvasWrap.appendChild(canvas);
+canvasWrap.appendChild(div);
+
+container.appendChild(canvasWrap);
+
+let textureText = new THREE.Texture(canvas);
+
+function changeCanvas() {
+	let ctx = canvas.getContext('2d');
+    ctx.font = '20pt Arial';
+    ctx.fillStyle = 'red';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'white';
+    ctx.fillRect(10, 10, canvas.width - 20, canvas.height - 20);
+    ctx.fillStyle = 'black';
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText(div.textContent, canvas.width / 2, canvas.height / 2);
+}
+
+div.style.position = "absolute";
+div.style.left = 0+'px';
+div.style.top = 0+'px';
+div.style.backgroundColor = 'rgb(255, 255, 255)';
+div.setAttribute('word-wrap', 'break-word');
+div.style.overflowY = 'scroll';
+
+//div.style.display = 'none';
+
+//canvas.style.display = 'none'; //so that we hide the actual HTML canvas. Which gets inserted after the THREEJS canvas
+//---------------IMAGE INFO-------------------//
+//image info
+let boxLength = (size+margin)*(numColomn+1);
+let boxHeight = (size+margin)*(numRow+1);
+let boxGeometry = new THREE.BoxGeometry( boxLength, boxHeight);
+//boxGeometry.translate(0, 0); //we translate the geometry so that the reference point is TOP-LEFT instead of centre.
+let boxMaterial = new THREE.MeshBasicMaterial( {
+			map: textureText,
+			transparent: false, //we need this to see the texture
+			color: 0xffddff 
+			//side: THREE.DoubleSide
+		});
+let imageInfoBox = new THREE.Mesh(boxGeometry, boxMaterial);
+
+imageInfoBox.position.x = group.position.x + boxLength + boxLength/2; //considering that position is relative to the center of the box
+imageInfoBox.position.y = group.position.y - boxHeight/2;
+imageInfoBox.position.z = -2;
+
+canvas.width = boxLength;
+canvas.height = boxHeight; 
+
+div.style.minHeight = boxHeight + 'px';
+div.style.minWidth = boxLength + 'px';
+div.style.left = boxLength+'px';
+//---------------ADDING ELEMENT TO SCENE-------------------//
+scene.add(group);
+scene.add(imageInfoBox);
 //-------------RoundedRectShape---------------//
 // Rounded rectangle
 const roundedRectShape = new THREE.Shape();
@@ -89,17 +200,13 @@ let DrawGrid = function()
 			});
 
 			let mesh = new THREE.Mesh( geometry, material);
-			mesh.position.set( x, y, -1);
+			mesh.position.set( x, y);
 			group.add( mesh );
 		}
 	}
 }
 
 DrawGrid();
-
-//========================CUSTOM FUNCTION =======================//
-
-
 
 //========================EVENT==================================//
 
@@ -121,6 +228,8 @@ window.addEventListener('resize', onWindowResize);
 //Game logic
 var update = function()
 {
+	changeCanvas();
+	textureText.needsUpdate = true;
 }
 
 //Draw scene
